58ae8009378abe7ca2a462f2672a806214f1a2d8
Felix Miller <phoenixarc.001@gmail.com>
2022-10-04 13:02:52 -0400

    add basic FUI Read/Write

---
+++ OMI Filetypes/Classes/Formats/FourjUserInterface.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMI.Formats.FUI
+{
+    public class FourjUserInterface
+    {
+        public UIComponent.fuiHeader Header;
+        public List<UIComponent.fuiTimeline> Timelines;
+        public List<UIComponent.fuiTimelineAction> TimelineActions;
+        public List<UIComponent.fuiShape> Shapes;
+        public List<UIComponent.fuiShapeComponent> ShapeComponents;
+        public List<UIComponent.fuiVert> Verts;
+        public List<UIComponent.fuiTimelineFrame> TimelineFrames;
+        public List<UIComponent.fuiTimelineEvent> TimelineEvents;
+        public List<UIComponent.fuiTimelineEventName> TimelineEventNames;
+        public List<UIComponent.fuiReference> References;
+        public List<UIComponent.fuiEdittext> Edittexts;
+        public List<UIComponent.fuiFontName> FontNames;
+        public List<UIComponent.fuiSymbol> Symbols;
+        public List<UIComponent.fuiImportAsset> ImportAssets;
+        public List<UIComponent.fuiBitmap> Bitmaps;
+        public List<UIComponent.fuiImage> Images = new List<UIComponent.fuiImage>();
+
+
+
+        public FourjUserInterface()
+        {
+            Header = new UIComponent.fuiHeader();
+            Timelines = new List<UIComponent.fuiTimeline>();
+            TimelineActions = new List<UIComponent.fuiTimelineAction>();
+            Shapes = new List<UIComponent.fuiShape>();
+            ShapeComponents = new List<UIComponent.fuiShapeComponent>();
+            Verts = new List<UIComponent.fuiVert>();
+            TimelineFrames = new List<UIComponent.fuiTimelineFrame>();
+            TimelineEvents = new List<UIComponent.fuiTimelineEvent>();
+            TimelineEventNames = new List<UIComponent.fuiTimelineEventName>();
+            References = new List<UIComponent.fuiReference>();
+            Edittexts = new List<UIComponent.fuiEdittext>();
+            FontNames = new List<UIComponent.fuiFontName>();
+            Symbols = new List<UIComponent.fuiSymbol>();
+            ImportAssets = new List<UIComponent.fuiImportAsset>();
+            Bitmaps = new List<UIComponent.fuiBitmap>();
+        }
+    }
+    public class UIComponent
+    {
+        #region Base FUI Structures
+
+        public class fuiHeader
+        {
+            public char[] Signature = new char[0x08];
+            public int ContentSize;
+            public char[] SwfFileName = new char[0x40];
+            public int fuiTimelineCount;
+            public int fuiTimelineEventNameCount;
+            public int fuiTimelineActionCount;
+            public int fuiShapeCount;
+            public int fuiShapeComponentCount;
+            public int fuiVertCount;
+            public int fuiTimelineFrameCount;
+            public int fuiTimelineEventCount;
+            public int fuiReferenceCount;
+            public int fuiEdittextCount;
+            public int fuiSymbolCount;
+            public int fuiBitmapCount;
+            public int imagesSize;
+            public int fuiFontNameCount;
+            public int fuiImportAssetCount;
+            public fuiRect frameSize = new fuiRect();
+        }
+        public class fuiTimeline
+        {
+            public int SymbolIndex;
+            public short FrameIndex;
+            public short FrameCount;
+            public short ActionIndex;
+            public short ActionCount;
+            public fuiRect Rectangle = new fuiRect();
+        }
+        public class fuiTimelineAction
+        {
+            public short ActionType;
+            public short Unknown;
+            public char[] StringArg0 = new char[0x40];
+            public char[] StringArg1 = new char[0x40];
+        }
+        public class fuiShape
+        {
+            public int Unknown;
+            public int ShapeComponentIndex;
+            public int ShapeComponentCount;
+            public fuiRect Rectangle = new fuiRect();
+        }
+        public class fuiShapeComponent
+        {
+            public fuiFillStyle FillInfo = new fuiFillStyle();
+            public int VertIndex;
+            public int VertCount;
+        }
+        public class fuiVert
+        {
+            public float X;
+            public float Y;
+        }
+        public class fuiTimelineFrame
+        {
+            public char[] FrameName = new char[0x40];
+            public int EventIndex;
+            public int EventCount;
+        }
+        public class fuiTimelineEvent
+        {
+            public short EventType;
+            public short ObjectType;
+            public short Unknown0;
+            public short Index;
+            public short Unknown1;
+            public short NameIndex;
+            public fuiMatrix matrix = new fuiMatrix();
+            public fuiColorTransform ColorTransform = new fuiColorTransform();
+            public fuiRGBA Color = new fuiRGBA();
+        }
+        public class fuiTimelineEventName
+        {
+            public char[] EventName = new char[0x40];
+        }
+        public class fuiReference
+        {
+            public int SymbolIndex;
+            public char[] Name = new char[0x40];
+            public int Index;
+        }
+        public class fuiEdittext
+        {
+            public int Unknown0;
+            public fuiRect Rectangle = new fuiRect();
+            public int FontID;
+            public float Unknown1;
+            public fuiRGBA Color = new fuiRGBA();
+            public int Unknown2;
+            public int Unknown3;
+            public int Unknown4;
+            public int Unknown5;
+            public int Unknown6;
+            public int Unknown7;
+            public char[] htmltextformat = new char[(0x100)];
+        }
+        public class fuiFontName
+        {
+            public int ID;
+            public char[] FontName = new char[0x40];
+            public int Unknown0;
+            public char[] Unknown1 = new char[0x40];
+            public int Unknown2;
+            public int Unknown3;
+            public char[] Unknown4 = new char[0x40];
+            public int Unknown5;
+            public int Unknown6;
+            public char[] Unknown7 = new char[0x2c];
+        }
+        public class fuiSymbol
+        {
+            public char[] SymbolName = new char[0x40];
+            public int ObjectType;
+            public int Index;
+        }
+        public class fuiImportAsset
+        {
+            public char[] Name = new char[0x40];
+        }
+        public class fuiBitmap
+        {
+            public int Unknown0;
+            public int ImageFormat;
+            public int Width;
+            public int Height;
+            public int Offset;
+            public int Size;
+            public int ZlibDataOffset;
+            public int Unknown1;
+        }
+
+        #endregion
+
+        #region Used structures in an FUI element
+
+        public class fuiRect
+        {
+            public float MinX;
+            public float MinY;
+            public float MaxX;
+            public float MaxY;
+        }
+        public class fuiRGBA
+        {
+            public byte R;
+            public byte G;
+            public byte B;
+            public byte A;
+        }
+        public class fuiMatrix
+        {
+            public float ScaleX;
+            public float ScaleY;
+            public float RotateSkew0;
+            public float RotateSkew1;
+            public float TranslationX;
+            public float TranslationY;
+        }
+        public class fuiColorTransform
+        {
+            public float RedMultTerm;
+            public float GreenMultTerm;
+            public float BlueMultTerm;
+            public float AlphaMultTerm;
+            public float RedAddTerm;
+            public float GreenAddTerm;
+            public float BlueAddTerm;
+            public float AlphaAddTerm;
+        }
+        public class fuiFillStyle
+        {
+            public int Type;
+            public fuiRGBA Color = new fuiRGBA();
+            public int BitmapIndex;
+            public fuiMatrix Matrix = new fuiMatrix();
+        }
+        public class fuiObject_eFuiObjectType
+        {
+            public int fuiObjectType;
+        }
+        public class fuiImage
+        {
+            public byte[] data;
+            public int Format;
+        }
+
+        #endregion
+
+    }
+}
---
+++ OMI Filetypes/Classes/Workers/FourjUIWorker/FourjUIWriter.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OMI.Formats.FUI;
+using OMI.utils;
+
+namespace OMI.Workers.FUI
+{
+    internal class FourjUIWriter : StreamDataWriter
+    {
+        FourjUserInterface _UIContainer;
+        public FourjUIWriter(FourjUserInterface container, bool useLittleEndian) : base(useLittleEndian)
+        {
+            _UIContainer = container;
+        }
+
+        public void Write(Stream s)
+        {
+            WriteBytes(s, Encoding.UTF8.GetBytes(_UIContainer.Header.Signature));
+            WriteInt(s, _UIContainer.Header.ContentSize);
+            WriteBytes(s, Encoding.UTF8.GetBytes(_UIContainer.Header.SwfFileName));
+            WriteInt(s, _UIContainer.Timelines.Count);
+            WriteInt(s, _UIContainer.TimelineEventNames.Count);
+            WriteInt(s, _UIContainer.TimelineActions.Count);
+            WriteInt(s, _UIContainer.Shapes.Count);
+            WriteInt(s, _UIContainer.ShapeComponents.Count);
+            WriteInt(s, _UIContainer.Verts.Count);
+            WriteInt(s, _UIContainer.TimelineFrames.Count);
+            WriteInt(s, _UIContainer.TimelineEvents.Count);
+            WriteInt(s, _UIContainer.References.Count);
+            WriteInt(s, _UIContainer.Edittexts.Count);
+            WriteInt(s, _UIContainer.Symbols.Count);
+            WriteInt(s, _UIContainer.Bitmaps.Count);
+            WriteInt(s, _UIContainer.Header.imagesSize);
+            WriteInt(s, _UIContainer.FontNames.Count);
+            WriteInt(s, _UIContainer.ImportAssets.Count);
+            WriteFloat(s, _UIContainer.Header.frameSize.MinX);
+            WriteFloat(s, _UIContainer.Header.frameSize.MaxX);
+            WriteFloat(s, _UIContainer.Header.frameSize.MinY);
+            WriteFloat(s, _UIContainer.Header.frameSize.MaxY);
+            foreach (UIComponent.fuiTimeline tl in _UIContainer.Timelines)
+            {
+                WriteInt(s, tl.SymbolIndex);
+                WriteShort(s, tl.FrameIndex);
+                WriteShort(s, tl.FrameCount);
+                WriteShort(s, tl.ActionIndex);
+                WriteShort(s, tl.ActionCount);
+                WriteFloat(s, tl.Rectangle.MinX);
+                WriteFloat(s, tl.Rectangle.MaxX);
+                WriteFloat(s, tl.Rectangle.MinY);
+                WriteFloat(s, tl.Rectangle.MaxY);
+            }
+            foreach (UIComponent.fuiTimelineAction tl in _UIContainer.TimelineActions)
+            {
+                WriteShort(s, tl.ActionType);
+                WriteShort(s, tl.Unknown);
+                WriteBytes(s, Encoding.UTF8.GetBytes(tl.StringArg0));
+                WriteBytes(s, Encoding.UTF8.GetBytes(tl.StringArg1));
+            }
+            foreach (UIComponent.fuiShape tl in _UIContainer.Shapes)
+            {
+                WriteInt(s, tl.Unknown);
+                WriteInt(s, tl.ShapeComponentIndex);
+                WriteInt(s, tl.ShapeComponentCount);
+                WriteFloat(s, tl.Rectangle.MinX);
+                WriteFloat(s, tl.Rectangle.MaxX);
+                WriteFloat(s, tl.Rectangle.MinY);
+                WriteFloat(s, tl.Rectangle.MaxY);
+            }
+            foreach (UIComponent.fuiShapeComponent tl in _UIContainer.ShapeComponents)
+            {
+                WriteInt(s, tl.FillInfo.Type);
+                WriteByte(s, (tl.FillInfo.Color.R));
+                WriteByte(s, (tl.FillInfo.Color.G));
+                WriteByte(s, (tl.FillInfo.Color.B));
+                WriteByte(s, (tl.FillInfo.Color.A));
+                WriteInt(s, tl.FillInfo.BitmapIndex);
+                WriteFloat(s, tl.FillInfo.Matrix.ScaleX);
+                WriteFloat(s, tl.FillInfo.Matrix.ScaleY);
+                WriteFloat(s, tl.FillInfo.Matrix.RotateSkew0);
+                WriteFloat(s, tl.FillInfo.Matrix.RotateSkew1);
+                WriteFloat(s, tl.FillInfo.Matrix.TranslationX);
+                WriteFloat(s, tl.FillInfo.Matrix.TranslationY);
+                WriteInt(s, tl.VertIndex);
+                WriteInt(s, tl.VertCount);
+            }
+            foreach (UIComponent.fuiVert tl in _UIContainer.Verts)
+            {
+                WriteFloat(s, (tl.X));
+                WriteFloat(s, (tl.Y));
+            }
+            foreach (UIComponent.fuiTimelineFrame tl in _UIContainer.TimelineFrames)
+            {
+                WriteBytes(s, Encoding.UTF8.GetBytes(tl.FrameName));
+                WriteInt(s, (tl.EventIndex));
+                WriteInt(s, (tl.EventCount));
+            }
+            foreach (UIComponent.fuiTimelineEvent tl in _UIContainer.TimelineEvents)
+            {
+                WriteShort(s, (tl.EventType));
+                WriteShort(s, (tl.ObjectType));
+                WriteShort(s, (tl.Unknown0));
+                WriteShort(s, (tl.Index));
+                WriteShort(s, (tl.Unknown1));
+                WriteShort(s, (tl.NameIndex));
+                WriteFloat(s, (tl.matrix.ScaleX));
+                WriteFloat(s, (tl.matrix.ScaleY));
+                WriteFloat(s, (tl.matrix.RotateSkew0));
+                WriteFloat(s, (tl.matrix.RotateSkew1));
+                WriteFloat(s, (tl.matrix.TranslationX));
+                WriteFloat(s, (tl.matrix.TranslationY));
+                WriteFloat(s, (tl.ColorTransform.RedMultTerm));
+                WriteFloat(s, (tl.ColorTransform.GreenMultTerm));
+                WriteFloat(s, (tl.ColorTransform.BlueMultTerm));
+                WriteFloat(s, (tl.ColorTransform.AlphaMultTerm));
+                WriteFloat(s, (tl.ColorTransform.RedAddTerm));
+                WriteFloat(s, (tl.ColorTransform.GreenAddTerm));
+                WriteFloat(s, (tl.ColorTransform.BlueAddTerm));
+                WriteFloat(s, (tl.ColorTransform.AlphaAddTerm));
+                //OutputBytes.AddRange(new byte[] { 0xff, 0xff, 0xff, 0xff});
+                WriteByte(s, (tl.Color.R));
+                WriteByte(s, (tl.Color.G));
+                WriteByte(s, (tl.Color.B));
+                WriteByte(s, (tl.Color.A));
+            }
+            foreach (UIComponent.fuiTimelineEventName tl in _UIContainer.TimelineEventNames)
+            {
+                WriteBytes(s, Encoding.UTF8.GetBytes(tl.EventName));
+            }
+            foreach (UIComponent.fuiReference tl in _UIContainer.References)
+            {
+                WriteInt(s, (tl.SymbolIndex));
+                WriteBytes(s, Encoding.UTF8.GetBytes(tl.Name));
+                WriteInt(s, (tl.Index));
+            }
+            foreach (UIComponent.fuiEdittext tl in _UIContainer.Edittexts)
+            {
+                WriteInt(s, (tl.Unknown0));
+                WriteFloat(s, (tl.Rectangle.MinX));
+                WriteFloat(s, (tl.Rectangle.MaxX));
+                WriteFloat(s, (tl.Rectangle.MinY));
+                WriteFloat(s, (tl.Rectangle.MaxY));
+                WriteInt(s, (tl.FontID));
+                WriteFloat(s, (tl.Unknown1));
+                WriteByte(s, (tl.Color.R));
+                WriteByte(s, (tl.Color.G));
+                WriteByte(s, (tl.Color.B));
+                WriteByte(s, (tl.Color.A));
+                WriteInt(s, (tl.Unknown2));
+                WriteInt(s, (tl.Unknown3));
+                WriteInt(s, (tl.Unknown4));
+                WriteInt(s, (tl.Unknown5));
+                WriteInt(s, (tl.Unknown6));
+                WriteInt(s, (tl.Unknown7));
+                WriteBytes(s, Encoding.UTF8.GetBytes(tl.htmltextformat));
+            }
+            foreach (UIComponent.fuiFontName tl in _UIContainer.FontNames)
+            {
+                WriteInt(s, (tl.ID));
+                WriteBytes(s, Encoding.UTF8.GetBytes(tl.FontName));
+                WriteInt(s, (tl.Unknown0));
+                WriteBytes(s, Encoding.UTF8.GetBytes(tl.Unknown1));
+                WriteInt(s, (tl.Unknown2));
+                WriteInt(s, (tl.Unknown3));
+                WriteBytes(s, Encoding.UTF8.GetBytes(tl.Unknown4));
+                WriteInt(s, (tl.Unknown5));
+                WriteInt(s, (tl.Unknown6));
+                WriteBytes(s, Encoding.UTF8.GetBytes(tl.Unknown7));
+            }
+            foreach (UIComponent.fuiSymbol tl in _UIContainer.Symbols)
+            {
+                WriteBytes(s, Encoding.UTF8.GetBytes(tl.SymbolName));
+                WriteInt(s, (tl.ObjectType));
+                WriteInt(s, (tl.Index));
+            }
+            foreach (UIComponent.fuiImportAsset tl in _UIContainer.ImportAssets)
+            {
+                WriteBytes(s, Encoding.UTF8.GetBytes(tl.Name));
+            }
+            foreach (UIComponent.fuiBitmap tl in _UIContainer.Bitmaps)
+            {
+                WriteInt(s, (tl.Unknown0));
+                WriteInt(s, (tl.ImageFormat));
+                WriteInt(s, (tl.Width));
+                WriteInt(s, (tl.Height));
+                WriteInt(s, (tl.Offset));
+                WriteInt(s, (tl.Size));
+                WriteInt(s, (tl.ZlibDataOffset));
+                WriteInt(s, (tl.Unknown1));
+            }
+            foreach (UIComponent.fuiImage img in _UIContainer.Images)
+            {
+                WriteBytes(s, img.data);
+            }
+        }
+
+        private void WriteByte(Stream stream, byte b)
+        {
+            WriteBytes(stream, new byte[] { b });
+        }
+    }
+}
