4b8722f489f993710fc99c5bf1c71ed10826b864
miku-666 <74728189+NessieHax@users.noreply.github.com>
2023-03-05 20:07:24 +0100

    Add behaviourFile type and workers

---
+++ OMI Filetypes Library/Classes/Workers/Behaviour/BehavioursReader.cs
@@ -0,0 +1,60 @@
+﻿using OMI;
+using OMI.Formats.Behaviour;
+using OMI.Workers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMI.Workers.Behaviour
+{
+    public class BehavioursReader : IDataFormatReader<BehaviourFile>, IDataFormatReader
+    {
+        public BehaviourFile FromFile(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                using (var fs = File.OpenRead(filename))
+                {
+                    return FromStream(fs);
+                }
+            }
+            throw new FileNotFoundException(filename);
+        }
+
+        public BehaviourFile FromStream(Stream stream)
+        {
+            BehaviourFile behaviourFile = new BehaviourFile();
+
+            var reader = new EndiannessAwareBinaryReader(stream, Encoding.ASCII, leaveOpen: true, Endianness.BigEndian);
+
+            _ = reader.ReadInt32();
+            int riderPosOverrideCount = reader.ReadInt32();
+            for (int i = 0; i < riderPosOverrideCount; i++)
+            {
+                short strlen = reader.ReadInt16();
+                string name = reader.ReadString(strlen);
+                var riderPositionOverride = new BehaviourFile.RiderPositionOverride(name);
+                int posOverideCount = reader.ReadInt32();
+                for (; 0 < posOverideCount; posOverideCount--)
+                {
+                    var posOverride = new BehaviourFile.RiderPositionOverride.PositionOverride();
+                    posOverride.EntityIsTamed = reader.ReadBoolean();
+                    posOverride.EntityHasSaddle = reader.ReadBoolean();
+                    posOverride.x = reader.ReadSingle();
+                    posOverride.y = reader.ReadSingle();
+                    posOverride.z = reader.ReadSingle();
+                    riderPositionOverride.overrides.Add(posOverride);
+                }
+                behaviourFile.entries.Add(riderPositionOverride);
+            }
+            return behaviourFile;
+        }
+
+        object IDataFormatReader.FromStream(Stream stream) => FromStream(stream);
+
+        object IDataFormatReader.FromFile(string filename) => FromFile(filename);
+    }
+}
---
+++ OMI Filetypes Library/Classes/Workers/Behaviour/BehavioursWriter.cs
@@ -0,0 +1,50 @@
+﻿using OMI.Formats.Behaviour;
+using System;
+using System.IO;
+using System.Text;
+
+namespace OMI.Workers.Behaviour
+{
+    public class BehavioursWriter : IDataFormatWriter
+    {
+        private BehaviourFile behaviourFile;
+
+        public static void Write(Stream stream, BehaviourFile file)
+        {
+            new BehavioursWriter(file).WriteToStream(stream);
+        }
+
+        public BehavioursWriter(BehaviourFile file)
+        {
+            behaviourFile = file;
+        }
+
+        public void WriteToFile(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDataFormatWriter.WriteToStream(Stream stream) => WriteToStream(stream);
+        public void WriteToStream(Stream stream)
+        {
+            var writer = new EndiannessAwareBinaryWriter(stream, Encoding.ASCII, leaveOpen: true, Endianness.BigEndian);
+
+            writer.Write(0);
+            writer.Write(behaviourFile.entries.Count);
+            foreach (var entry in behaviourFile.entries)
+            {
+                writer.Write((short)entry.name.Length);
+                writer.WriteString(entry.name);
+                writer.Write(entry.overrides.Count);
+                foreach (var posOverride in entry.overrides)
+                {
+                    writer.Write(posOverride.EntityIsTamed);
+                    writer.Write(posOverride.EntityHasSaddle);
+                    writer.Write(posOverride.x);
+                    writer.Write(posOverride.y);
+                    writer.Write(posOverride.z);
+                }
+            }
+        }
+    }
+}
