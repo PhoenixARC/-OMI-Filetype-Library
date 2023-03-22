bc4bf867d0c5884541b7f72a46f9af77da8dea2b
miku-666 <74728189+NessieHax@users.noreply.github.com>
2023-03-22 17:49:53 +0100

    Update COLFileReader and remove performance measuring

--- OMI Filetypes Library/Classes/Workers/ColorWorker/COLFileReader.cs
+++ OMI Filetypes Library/Classes/Workers/ColorWorker/COLFileReader.cs
@@ -24,16 +24,15 @@ namespace OMI.Workers.Color
         public ColorContainer FromStream(Stream s)
         {
             var colorContainer = new ColorContainer();
-            using (var reader = new EndiannessAwareBinaryReader(s, Endianness.BigEndian))
+            using (var reader = new EndiannessAwareBinaryReader(s, Encoding.ASCII, leaveOpen: true, Endianness.BigEndian))
             {
-                Stopwatch stopwatch = Stopwatch.StartNew();
                 colorContainer.Version = reader.ReadInt32();
                 int NumOfColors = reader.ReadInt32();
                 for (int i = 0; i < NumOfColors; i++)
                 {
                     var col = new ColorContainer.Color();
                     short length = reader.ReadInt16();
-                    col.Name = reader.ReadString(length, Encoding.ASCII);
+                    col.Name = reader.ReadString(length);
                     col.ColorPallette = System.Drawing.Color.FromArgb(reader.ReadInt32());
                     colorContainer.Colors.Add(col);
                 }
@@ -42,14 +41,12 @@ namespace OMI.Workers.Color
                 {
                     var col = new ColorContainer.WaterColor();
                     short length = reader.ReadInt16();
-                    col.Name = reader.ReadString(length, Encoding.ASCII);
+                    col.Name = reader.ReadString(length);
                     col.SurfaceColor = System.Drawing.Color.FromArgb(reader.ReadInt32());
                     col.UnderwaterColor = System.Drawing.Color.FromArgb(reader.ReadInt32());
                     col.FogColor = System.Drawing.Color.FromArgb(reader.ReadInt32());
                     colorContainer.WaterColors.Add(col);
                 }
-                stopwatch.Stop();
-                Debug.WriteLine("Completed in: " + stopwatch.Elapsed, category: nameof(COLFileReader.FromStream));
             }
             return colorContainer;
         }
